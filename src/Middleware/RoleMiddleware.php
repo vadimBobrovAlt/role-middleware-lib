3ef824b03d21e00a9e4b59c8ea7305288374902b
Vadim Bobrov <vadim762905@gmail.com>
2024-10-07 12:11:55 +0700

    add user exists check

--- src/Middleware/RoleMiddleware.php
+++ src/Middleware/RoleMiddleware.php
@@ -10,14 +10,18 @@ class RoleMiddleware
 {
     /**
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string  ...$roles
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @param string ...$roles
      * @return mixed
      */
     public function handle(Request $request, Closure $next, ...$roles)
     {
         $user = Auth::user();
+        if (!$user) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+
         foreach ($roles as $role) {
             if ($user->roles && in_array($role, $user->roles)) {
                 return $next($request);
